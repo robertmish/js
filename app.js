824a1db5d66c314bb794b58bde47a1b3ac6f6514
robertmish <robertmish@gmail.com>
2023-10-26 19:26:46 +0200

    Javascript class #1

---
+++ app.js
@@ -0,0 +1,36 @@
+// Javascript data types
+
+// Strings
+const name = "Robert";
+
+// Number
+const age = 34;
+
+// Bolean
+let status = true;
+
+// Null
+const totalNumber = null;
+
+// Symbol
+
+const heart = Symbol("Robert");
+
+// Printing the values of variables
+
+const x = Symbol("hey");
+
+console.log(x.description);
+
+// Objects
+
+const userInfo = {
+  name: "robert",
+  age: 34,
+  status: true,
+  hobbie: null,
+};
+
+const sentence = `Robert is ${age} and his marriage status is ${status}`;
+
+console.log(sentence);
